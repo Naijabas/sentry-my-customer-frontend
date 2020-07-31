0c492aa057a217745ca7bda3f5714140ebef1fb2
Chinaza <obiekweagnesmary@gmail.com>
2020-07-31 08:58:36 +0100

    test

---
+++ resources/views/backend/payment_details/bank.blade.php
@@ -0,0 +1,31 @@
+<!-- @extends('backend.payment_details.layout.app')
+
+@section('content')
+<div>
+    <h4 class="font-weight-bold text-center">Payment</h4>
+
+    <div class="py-3">
+        <div class="lead d-flex justify-content-between align-items-center">
+            <span>Total</span>
+            <span>$6,700</span>
+        </div>
+        <div class="row p-2 text-center">
+            <a href="" aria-disabled="true" disabled class="col w-1/2 rounded-left py-3 bg-primary text-white">
+                <img src="{{ asset('frontend/assets/images/payment/vector.svg')}}" alt="" class="inline"> Pay with
+                Card</a>
+            <a href="" class="col w-1/2 rounded-right py-3 btn-light text-dark">Pay with Transfer</a>
+        </div>
+    </div>
+ <div class="bank">
+             <p class="first"> Account Name:</p> 00932 <br><br>
+              Account Number: Bola <br>
+              Bank: FCMB
+
+           
+           
+
+        </div>
+    
+</div>
+@endsection
+ -->
